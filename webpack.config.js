09533f4a55b9d8e85055da81711f71bb504ee24d
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-02-26 15:52:07 +0100

    Update to Webpack 4.0.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,52 @@
-module.exports = {
-  entry: './src/app.jsx',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/public'
-  },
-  module: {
-    rules: [{
-      test: /\.js|jsx$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    }]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  devServer: {
-    contentBase: __dirname + '/public',
-    host: '0.0.0.0',
-    historyApiFallback: true
-  },
-  devtool: 'cheap-module-eval-source-map'
-}
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const cssExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.jsx',
+    output: {
+      path: path.join(__dirname, 'public', 'assets'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.jsx|js$/,
+        exclude: /node_modules/
+      }, {
+        test: /\.s|css$/,
+        use: cssExtract.extract({
+          use: [
+            { 
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      }]
+    },
+    plugins: [
+      cssExtract
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/assets/'
+    }
+  };
+};
