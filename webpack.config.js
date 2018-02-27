fd296cbc5d5eea8f364a2714687d5a312d0d402b
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-02-27 19:02:05 +0100

    Add 0.0.0.0 host to dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -46,6 +46,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
+      host: "0.0.0.0",
       publicPath: '/assets/'
     }
   };
